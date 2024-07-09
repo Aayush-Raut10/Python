bc768c5e5cba3c568f64ab077a55621d31892e2c
Aayush Raut <aayushraut209@gmail.com>
2024-07-09 18:34:37 +0545

    Add files via upload

---
+++ Sum _of _two _numbers.py
@@ -0,0 +1,10 @@
+#Getting two numbers as input from user
+
+num1 = int(input("Enter number1: "))
+num2 = int(input("Enter numbr2: "))
+
+#Adding two numbers
+sum = num1 + num2
+
+#Displayng sum
+print("The sum of ",num1,"and",num2,"is",sum)
