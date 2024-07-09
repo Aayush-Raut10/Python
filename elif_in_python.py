2a6760765e89116d381a10ee83abcd996fc277d4
Aayush Raut <aayushraut209@gmail.com>
2024-07-09 19:17:25 +0545

    Add files via upload
    elif in python

---
+++ elif_in_python.py
@@ -0,0 +1,18 @@
+#Getting maks input from user
+
+marks = int(input("Enter the marks = "))
+
+if(marks>=90):
+    print(" you got grade A+")
+
+elif(marks>=80 and marks<90):
+    print("You got grade A")
+
+elif(marks>=70 and marks<80):
+    print("You got grade B+")
+
+elif(marks>=60 and marks<70):
+    print("You got grade B")
+
+else:
+    print("You should study hard")
